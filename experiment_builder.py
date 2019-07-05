found bug in f metric computation

experiment_builder.py
@@ -158,7 +158,7 @@ class ExperimentBuilder(nn.Module):
             average='weighted'
         )
 
-        stats[type_key + '_f_score'] = f1score_overall
+        stats[type_key + '_f_score'].append(f1score_overall)
 
         f1scores = f1_score(
             y_true.cpu().detach().numpy(),
@@ -178,9 +178,9 @@ class ExperimentBuilder(nn.Module):
         )
 
         for i in range(len(f1scores)):
-            stats[type_key + '_f_score_' + LABEL_MAPPING[i]] = f1scores[i]
-            stats[type_key + '_precision_' + LABEL_MAPPING[i]] = precision[i]
-            stats[type_key + '_recall_' + LABEL_MAPPING[i]] = recall[i]
+            stats[type_key + '_f_score_' + LABEL_MAPPING[i]].append(f1scores[i])
+            stats[type_key + '_precision_' + LABEL_MAPPING[i]].append(precision[i])
+            stats[type_key + '_recall_' + LABEL_MAPPING[i]].append(recall[i])
 
     def save_best_performing_model(self, epoch_stats, epoch_idx):
         metrics_to_aggregate = [epoch_stats[item] for item in ['valid_f_score_abusive', 'valid_f_score_hateful']]
@@ -197,7 +197,7 @@ class ExperimentBuilder(nn.Module):
                               for key, value in stats.items() if key != 'epoch'])
         epoch_elapsed_time = (time.time() - start_time) / 60  # calculate time taken for epoch
         epoch_elapsed_time = "{:.4f}".format(epoch_elapsed_time)
-        print("===Epoch {}===\n{}===Elapsed time: {} mins===".format(index, out_string, epoch_elapsed_time))
+        print("\n===Epoch {}===\n{}===Elapsed time: {} mins===".format(index, out_string, epoch_elapsed_time))
 
     def run_experiment(self, round_param=4):
         """
@@ -221,6 +221,7 @@ class ExperimentBuilder(nn.Module):
                     pbar_val.update(1)  # add 1 step to the progress bar
                     pbar_val.set_description("loss: {:.4f}, accuracy: {:.4f}".format(epoch_stats['valid_loss'][-1],
                                                                                      epoch_stats['valid_acc'][-1]))
+
             self.save_best_performing_model(epoch_stats, epoch_idx)
 
             # save to train stats
@@ -242,7 +243,7 @@ class ExperimentBuilder(nn.Module):
 
         ### EXPERIMENTS END ###
         # save train statistics
-        prepare_output_file(filename="{}/{}".format(self.experiment_folder, "train_statistics.csv"),
+        prepare_output_file(filename="{}/{}".format(self.experiment_folder, "train_statistics_{}.csv".format(self.seed)),
                             output=list(train_stats.values()))
 
         print("Generating test set evaluation metrics")
@@ -264,6 +265,6 @@ class ExperimentBuilder(nn.Module):
         merge_dict = dict(list(test_stats.items()) +
                           list(train_stats["epoch_{}".format(self.best_val_model_idx)].items()))
 
-        prepare_output_file(filename="{}/{}".format(self.experiment_folder, "results.csv"),
+        prepare_output_file(filename="{}/{}".format(self.experiment_folder, "results.csv".format(self.seed)),
                             output=[merge_dict])
         return train_stats, test_stats

models/cnn.py
@@ -6,116 +6,116 @@ from models.base import Network
 
 DILATION_PARAM = 1.4
 WEIGHT_DECAY = 1e-4
+#
 
+# class CNN(nn.Module):
+#     def __init__(self, input_shape, num_output_classes, num_filters, num_layers, dropout=.5, use_bias=False):
+#         """
+#         Initializes a convolutional network module object.
+#         :param input_shape: The shape of the inputs going in to the network.
+#         :param dim_reduction_type: The type of dimensionality reduction to apply after each convolutional stage, should be one of ['max_pooling', 'avg_pooling', 'strided_convolution', 'dilated_convolution']
+#         :param num_output_classes: The number of outputs the network should have (for classification those would be the number of classes)
+#         :param num_filters: Number of filters used in every conv layer, except dim reduction stages, where those are automatically infered.
+#         :param num_layers: Number of conv layers (excluding dim reduction stages)
+#         :param use_bias: Whether our convolutions will use a bias.
+#         """
+#         super(CNN, self).__init__()
+#         # set up class attributes useful in building the network and inference
+#         self.input_shape = input_shape
+#         self.num_filters = num_filters
+#         self.num_output_classes = num_output_classes
+#         self.use_bias = use_bias
+#         self.num_layers = num_layers
+#         self.drop = nn.Dropout(p=dropout, inplace=False)
+#
+#         # initialize a module dict, which is effectively a dictionary that can collect layers and integrate them into pytorch
+#         self.layer_dict = nn.ModuleDict()
+#
+#         # build the network
+#         self.build_module()
+#
+#     def build_module(self):
+#         """
+#         Builds network whilst automatically inferring shapes of layers.
+#         """
+#         x = torch.zeros((self.input_shape))  # create dummy inputs to be used to infer shapes of layers
+#         out = x
+#         out = out.permute([0, 2, 1])
+#         print("Building basic block of ConvolutionalNetwork using input shape", out.shape)
+#
+#         context_list = []
+#         for i in range(self.num_layers):  # for number of layers times
+#             if i > 0:
+#                 # to give every layer access to all prev layers (DenseNet connectivity)
+#                 out = torch.cat(context_list, dim=1)
+#             dilation = int(DILATION_PARAM**i)
+#             # dilation = 1
+#             self.layer_dict['conv_{}'.format(i)] = nn.Conv1d(in_channels=out.shape[1],
+#                                                              # add a conv layer in the module dict
+#                                                              kernel_size=3,
+#                                                              out_channels=self.num_filters,
+#                                                              padding=dilation,
+#                                                              bias=False,
+#                                                              dilation=dilation)
+#
+#             out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
+#             self.layer_dict['batch_norm_{}'.format(i)] = nn.BatchNorm1d(num_features=out.shape[1])
+#             out = self.layer_dict['batch_norm_{}'.format(i)](out)
+#             out = F.leaky_relu(out)
+#             out = self.drop(out)
+#             context_list.append(out)
+#
+#         out = torch.cat(context_list, dim=1)
+#         out = F.avg_pool1d(out, out.shape[-1])
+#         out = out.view(out.shape[0], -1)
+#
+#         self.logit_linear_layer = nn.Linear(in_features=out.shape[1],  # add a linear layer
+#                                             out_features=self.num_output_classes,
+#                                             bias=self.use_bias)
+#
+#         out = self.logit_linear_layer(out)  # apply linear layer on flattened inputs
+#         print("Block is built, output volume is", out.shape)
+#         return out
+#
+#     def forward(self, x):
+#         """
+#         Forward propages the network given an input batch
+#         :param x: Inputs x (b, c, h, w)
+#         :return: preds (b, num_classes)
+#         """
+#         out = x
+#         out = out.permute([0, 2, 1])
+#         context_list = []
+#         for i in range(self.num_layers):  # for number of layers times
+#             if i > 0:
+#                 out = torch.cat(context_list, dim=1)
+#
+#             out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
+#             out = self.layer_dict['batch_norm_{}'.format(i)](out)
+#             out = F.leaky_relu(out)
+#             out = self.drop(out)
+#             context_list.append(out)
+#
+#         out = torch.cat(context_list, dim=1)
+#         out = F.avg_pool1d(out, out.shape[-1])
+#         out = out.view(out.shape[0], -1)  # flatten outputs from (b, c, h, w) to (b, c*h*w)
+#         out = self.logit_linear_layer(out)  # pass through a linear layer to get logits/preds
+#
+#         return out
+#
+#     def reset_parameters(self):
+#         """
+#         Re-initialize the network parameters.
+#         """
+#         for item in self.layer_dict.children():
+#             try:
+#                 item.reset_parameters()
+#             except:
+#                 pass
+#
+#         self.logit_linear_layer.reset_parameters()
 
-class CNN(nn.Module):
-    def __init__(self, input_shape, num_output_classes, num_filters, num_layers, dropout=.5, use_bias=False):
-        """
-        Initializes a convolutional network module object.
-        :param input_shape: The shape of the inputs going in to the network.
-        :param dim_reduction_type: The type of dimensionality reduction to apply after each convolutional stage, should be one of ['max_pooling', 'avg_pooling', 'strided_convolution', 'dilated_convolution']
-        :param num_output_classes: The number of outputs the network should have (for classification those would be the number of classes)
-        :param num_filters: Number of filters used in every conv layer, except dim reduction stages, where those are automatically infered.
-        :param num_layers: Number of conv layers (excluding dim reduction stages)
-        :param use_bias: Whether our convolutions will use a bias.
-        """
-        super(CNN, self).__init__()
-        # set up class attributes useful in building the network and inference
-        self.input_shape = input_shape
-        self.num_filters = num_filters
-        self.num_output_classes = num_output_classes
-        self.use_bias = use_bias
-        self.num_layers = num_layers
-        self.drop = nn.Dropout(p=dropout, inplace=False)
-
-        # initialize a module dict, which is effectively a dictionary that can collect layers and integrate them into pytorch
-        self.layer_dict = nn.ModuleDict()
-
-        # build the network
-        self.build_module()
-
-    def build_module(self):
-        """
-        Builds network whilst automatically inferring shapes of layers.
-        """
-        x = torch.zeros((self.input_shape))  # create dummy inputs to be used to infer shapes of layers
-        out = x
-        out = out.permute([0, 2, 1])
-        print("Building basic block of ConvolutionalNetwork using input shape", out.shape)
-
-        context_list = []
-        for i in range(self.num_layers):  # for number of layers times
-            if i > 0:
-                # to give every layer access to all prev layers (DenseNet connectivity)
-                out = torch.cat(context_list, dim=1)
-            dilation = int(DILATION_PARAM**i)
-            # dilation = 1
-            self.layer_dict['conv_{}'.format(i)] = nn.Conv1d(in_channels=out.shape[1],
-                                                             # add a conv layer in the module dict
-                                                             kernel_size=3,
-                                                             out_channels=self.num_filters,
-                                                             padding=dilation,
-                                                             bias=False,
-                                                             dilation=dilation)
-
-            out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
-            self.layer_dict['batch_norm_{}'.format(i)] = nn.BatchNorm1d(num_features=out.shape[1])
-            out = self.layer_dict['batch_norm_{}'.format(i)](out)
-            out = F.leaky_relu(out)
-            out = self.drop(out)
-            context_list.append(out)
-
-        out = torch.cat(context_list, dim=1)
-        out = F.avg_pool1d(out, out.shape[-1])
-        out = out.view(out.shape[0], -1)
-
-        self.logit_linear_layer = nn.Linear(in_features=out.shape[1],  # add a linear layer
-                                            out_features=self.num_output_classes,
-                                            bias=self.use_bias)
-
-        out = self.logit_linear_layer(out)  # apply linear layer on flattened inputs
-        print("Block is built, output volume is", out.shape)
-        return out
-
-    def forward(self, x):
-        """
-        Forward propages the network given an input batch
-        :param x: Inputs x (b, c, h, w)
-        :return: preds (b, num_classes)
-        """
-        out = x
-        out = out.permute([0, 2, 1])
-        context_list = []
-        for i in range(self.num_layers):  # for number of layers times
-            if i > 0:
-                out = torch.cat(context_list, dim=1)
-
-            out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
-            out = self.layer_dict['batch_norm_{}'.format(i)](out)
-            out = F.leaky_relu(out)
-            out = self.drop(out)
-            context_list.append(out)
-
-        out = torch.cat(context_list, dim=1)
-        out = F.avg_pool1d(out, out.shape[-1])
-        out = out.view(out.shape[0], -1)  # flatten outputs from (b, c, h, w) to (b, c*h*w)
-        out = self.logit_linear_layer(out)  # pass through a linear layer to get logits/preds
-
-        return out
-
-    def reset_parameters(self):
-        """
-        Re-initialize the network parameters.
-        """
-        for item in self.layer_dict.children():
-            try:
-                item.reset_parameters()
-            except:
-                pass
-
-        self.logit_linear_layer.reset_parameters()
-
-
+#
 # class CNN(nn.Module):
 #     def __init__(self, input_shape, num_output_classes, num_filters, num_layers, dim_reduction_type='max_pooling', use_bias=False):
 #         """
@@ -245,8 +245,8 @@ class CNN(nn.Module):
 #                 pass
 #
 #         self.logit_linear_layer.reset_parameters()
+
 #
-# #
 # class CharacterCNN(Network):
 #     def __init__(self, input_shape, num_output_classes, num_filters_character, num_filters, num_layers, kernel_size=3, use_bias=False):
 #         """
@@ -382,11 +382,110 @@ class CNN(nn.Module):
 #
 #         self.logit_linear_layer.reset_parameters()
 
+class CNN(nn.Module):
+    def __init__(self, input_shape, num_output_classes, num_filters, num_layers, dropout=.5, use_bias=False):
+        """
+        Initializes a convolutional network module object.
+        :param input_shape: The shape of the inputs going in to the network.
+        :param dim_reduction_type: The type of dimensionality reduction to apply after each convolutional stage, should be one of ['max_pooling', 'avg_pooling', 'strided_convolution', 'dilated_convolution']
+        :param num_output_classes: The number of outputs the network should have (for classification those would be the number of classes)
+        :param num_filters: Number of filters used in every conv layer, except dim reduction stages, where those are automatically infered.
+        :param num_layers: Number of conv layers (excluding dim reduction stages)
+        :param use_bias: Whether our convolutions will use a bias.
+        """
+        super(CNN, self).__init__()
+        # set up class attributes useful in building the network and inference
+        self.input_shape = input_shape
+        self.num_filters = num_filters
+        self.num_output_classes = num_output_classes
+        self.use_bias = use_bias
+        self.num_layers = num_layers
+        self.drop = nn.Dropout(p=dropout, inplace=False)
 
-def word_cnn(input_shape, dropout):
+        # initialize a module dict, which is effectively a dictionary that can collect layers and integrate them into pytorch
+        self.layer_dict = nn.ModuleDict()
+
+        # build the network
+        self.build_module()
+
+    def build_module(self):
+        """
+        Builds network whilst automatically inferring shapes of layers.
+        """
+        x = torch.zeros((self.input_shape))  # create dummy inputs to be used to infer shapes of layers
+        out = x
+        out = out.permute([0, 2, 1])
+        print("Building basic block of ConvolutionalNetwork using input shape", out.shape)
+        #
+        # context_list = []
+        # for i in range(self.num_layers):  # for number of layers times
+        #     if i > 0:
+        #         # to give every layer access to all prev layers (DenseNet connectivity)
+        #         out = torch.cat(context_list, dim=1)
+            # dilation = int(DILATION_PARAM**i)
+            # # dilation = 1
+            # self.layer_dict['conv_{}'.format(i)] = nn.Conv1d(in_channels=out.shape[1],
+            #                                                  # add a conv layer in the module dict
+            #                                                  kernel_size=3,
+            #                                                  out_channels=self.num_filters,
+            #                                                  padding=dilation,
+            #                                                  bias=False,
+            #                                                  dilation=dilation)
+
+            # out = self.layer_dict['conv_{}'.format(i)](out)  # use layer on inputs to get an output
+            # self.layer_dict['batch_norm_{}'.format(i)] = nn.BatchNorm1d(num_features=out.shape[1])
+            # out = self.layer_dict['batch_norm_{}'.format(i)](out)
+        #     out = F.leaky_relu(out)
+        #     out = self.drop(out)
+        #     context_list.append(out)
+        #
+        # out = torch.cat(context_list, dim=1)
+        out = F.avg_pool1d(out, out.shape[-1])
+        out = out.view(out.shape[0], -1)
+        for i in range(self.num_layers):
+            if i > 0:
+                out = F.leaky_relu(out)
+            self.layer_dict['linear_{}'.format(i)] = nn.Linear(in_features=out.shape[1],  # add a linear layer
+                                                out_features=self.num_output_classes,
+                                                bias=self.use_bias)
+            out = self.layer_dict['linear_{}'.format(i)](out)
+
+        # out = self.logit_linear_layer(out)  # apply linear layer on flattened inputs
+        print("Block is built, output volume is", out.shape)
+        return out
+
+    def forward(self, x):
+        """
+        Forward propages the network given an input batch
+        :param x: Inputs x (b, c, h, w)
+        :return: preds (b, num_classes)
+        """
+        out = x
+        out = out.permute([0, 2, 1])
+        out = F.avg_pool1d(out, out.shape[-1])
+        out = out.view(out.shape[0], -1)
+        for i in range(self.num_layers):
+            if i > 0:
+                out = F.leaky_relu(out)
+            out = self.layer_dict['linear_{}'.format(i)](out)
+        return out
+
+    def reset_parameters(self):
+        """
+        Re-initialize the network parameters.
+        """
+        for item in self.layer_dict.children():
+            try:
+                item.reset_parameters()
+            except:
+                pass
+
+        # self.logit_linear_layer.reset_parameters()
+
+def word_cnn(input_shape, dropout=.5):
     model = CNN(num_output_classes=4,
                 num_filters=32,
-                num_layers=3,
+                num_layers=1,
                 input_shape=input_shape)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), weight_decay=WEIGHT_DECAY)